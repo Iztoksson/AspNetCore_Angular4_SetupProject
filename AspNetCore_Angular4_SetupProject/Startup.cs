4aae37c1961ddece79756725eba123fac6e9ea3d
Iztoksson <iztok.sebic@gmail.com>
2017-05-27 11:21:30 +0200

    Init commit.

---
+++ AspNetCore_Angular4_SetupProject/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore_Angular4_SetupProject
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            // POI: If a route is requested which is not handled by WebApi redirect it to index.html.
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404
+                    && !System.IO.Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            // POI: We need other static files to be served.
+            app.UseStaticFiles(); 
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ AspNetCore_Angular4_SetupProject/gulpfile.js
@@ -0,0 +1,71 @@
+﻿/// <binding AfterBuild='default' />
+// POI: !! Must be absolute first line in this document!! 
+//      This line tells VS to run task 'default' after each build so app files are copied over to wwwroot folder.
+
+var gulp = require('gulp');
+
+var libs = './wwwroot/libs/';
+
+// POI: Copy everything from ./app folder into wwwroot folder.
+var paths = {
+    scripts: ['app/**/*.js', 'app/**/*.ts', 'app/**/*.map'],
+};
+// POI: This 
+gulp.task('default', function () {
+    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/app'))
+});
+
+// POI: Restore or init required packages and dependency modules. Run this on-demand, don't put it in each "AfterBuild" (see first POI).
+gulp.task('restore:core-js', function () {
+    gulp.src([
+        'node_modules/core-js/client/*.js'
+    ]).pipe(gulp.dest(libs + 'core-js'));
+});
+gulp.task('restore:zone.js', function () {
+    gulp.src([
+        'node_modules/zone.js/dist/*.js'
+    ]).pipe(gulp.dest(libs + 'zone.js'));
+});
+gulp.task('restore:reflect-metadata', function () {
+    gulp.src([
+        'node_modules/reflect-metadata/reflect.js'
+    ]).pipe(gulp.dest(libs + 'reflect-metadata'));
+});
+gulp.task('restore:systemjs', function () {
+    gulp.src([
+        'node_modules/systemjs/dist/*.js'
+    ]).pipe(gulp.dest(libs + 'systemjs'));
+});
+gulp.task('restore:rxjs', function () {
+    gulp.src([
+        'node_modules/rxjs/**/*.js'
+    ]).pipe(gulp.dest(libs + 'rxjs'));
+});
+gulp.task('restore:angular-in-memory-web-api', function () {
+    gulp.src([
+        'node_modules/angular-in-memory-web-api/**/*.js'
+    ]).pipe(gulp.dest(libs + 'angular-in-memory-web-api'));
+});
+
+gulp.task('restore:angular', function () {
+    gulp.src([
+        'node_modules/@angular/**/*.js'
+    ]).pipe(gulp.dest(libs + '@angular'));
+});
+
+gulp.task('restore:bootstrap', function () {
+    gulp.src([
+        'node_modules/bootstrap/dist/**/*.*'
+    ]).pipe(gulp.dest(libs + 'bootstrap'));
+});
+
+gulp.task('restore', [
+    'restore:core-js',
+    'restore:zone.js',
+    'restore:reflect-metadata',
+    'restore:systemjs',
+    'restore:rxjs',
+    'restore:angular-in-memory-web-api',
+    'restore:angular',
+    'restore:bootstrap',
+]);
